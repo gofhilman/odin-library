a541d0e56f60c915705d4904fecb26b63770a6e0
Hilman Fikry <hilman12fikry@gmail.com>
2025-03-11 11:44:32 +0700

    Refactor using class structure
    The code looks cleaner compared to the previous one

--- script.js
+++ script.js
@@ -1,68 +1,66 @@
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
-function createTableHeader() {
-    if (myLibrary.length == 1) {
-        const headerRow = document.createElement("tr");
-        const columnHeaders = ["Title", "Author", "Pages", "Read", "Action"];
-        columnHeaders.forEach(columnHeader => {
-            const header = document.createElement("th");
-            header.textContent = columnHeader;
-            headerRow.appendChild(header);
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    addBookToLibrary() {
+        myLibrary.push(this);
+    }
+    resetValues() {
+        bookTitle.value = '';
+        bookAuthor.value = '';
+        bookPages.value = null;
+        bookRead.checked = false;
+    }
+    static #createTableHeader() {
+        if (myLibrary.length == 1) {
+            const headerRow = document.createElement("tr");
+            const columnHeaders = ["Title", "Author", "Pages", "Read", "Action"];
+            columnHeaders.forEach(columnHeader => {
+                const header = document.createElement("th");
+                header.textContent = columnHeader;
+                headerRow.appendChild(header);
+            });
+            tableHeader.appendChild(headerRow);
+        }
+    }
+    static displayBooks() {
+        Book.#createTableHeader();
+        let book = myLibrary[myLibrary.length-1];
+    
+        // Display the book title, author, and page number
+        const bodyRow = document.createElement("tr");
+        const bookData = [book.title, book.author, book.pages];
+        bookData.forEach(bookInfo => {
+                const cell = document.createElement("td");
+                cell.textContent = bookInfo;
+                bodyRow.appendChild(cell);
         });
-        tableHeader.appendChild(headerRow);
+    
+        // Display a tickbox for the book read
+        let cell = document.createElement("td");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        if (book.read) checkbox.checked = true;
+        cell.appendChild(checkbox);
+        bodyRow.appendChild(cell);
+    
+        // Display a button to remove the book
+        cell = document.createElement("td");
+        const removeButton = document.createElement("button");
+        removeButton.textContent = 'remove';
+        cell.appendChild(removeButton);
+        bodyRow.appendChild(cell);
+        tableBody.appendChild(bodyRow);
+        removeButton.addEventListener('click',() => {
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+            tableBody.removeChild(bodyRow);
+            if (myLibrary.length == 0) tableHeader.replaceChildren();
+        });
     }
 }
-
-function displayBooks() {
-    createTableHeader();
-    let book = myLibrary[myLibrary.length-1];
-
-    // Display the book title, author, and page number
-    const bodyRow = document.createElement("tr");
-    const bookData = [book.title, book.author, book.pages];
-    bookData.forEach(bookInfo => {
-            const cell = document.createElement("td");
-            cell.textContent = bookInfo;
-            bodyRow.appendChild(cell);
-    });
-
-    // Display a tickbox for the book read
-    let cell = document.createElement("td");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    if (book.read) checkbox.checked = true;
-    cell.appendChild(checkbox);
-    bodyRow.appendChild(cell);
-
-    // Display a button to remove the book
-    cell = document.createElement("td");
-    const removeButton = document.createElement("button");
-    removeButton.textContent = 'remove';
-    cell.appendChild(removeButton);
-    bodyRow.appendChild(cell);
-    tableBody.appendChild(bodyRow);
-    removeButton.addEventListener('click',() => {
-        myLibrary.splice(myLibrary.indexOf(book), 1);
-        tableBody.removeChild(bodyRow);
-        if (myLibrary.length == 0) tableHeader.replaceChildren();
-    });
-}
-
-function resetValues() {
-    bookTitle.value = '';
-    bookAuthor.value = '';
-    bookPages.value = null;
-    bookRead.checked = false;
-}  
 
 const myLibrary = [];
 const tableHeader = document.querySelector("thead");
@@ -84,7 +82,7 @@ confirmData.addEventListener('click', (event) => {
     event.preventDefault();
     bookDialog.close(); 
     let book = new Book(bookTitle.value , bookAuthor.value, bookPages.value, bookRead.checked);
-    addBookToLibrary(book);
-    displayBooks();
-    resetValues();
+    book.addBookToLibrary();
+    Book.displayBooks();
+    book.resetValues();
 })
